4bc31a545a38fa5a5246bc527d0adaf25c9b24bf
Matty Wang <mattywang7@gmail.com>
2021-03-24 13:52:12 +0000

    static Logout

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Logout from "./user/Logout";
+
+
+
+export default function App() {
+    return <Logout user={"Daniel"} />
+}
---
+++ src/user/Logout.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**
+ * Display the currently logged in user, and a button to log out.
+ * Take a user prop, which used to display the currently logged-in user
+ *
+ * Here use destructuring in order to extract the user key from the props object.
+ * React passes all component props, in a single object, as the first argument to a function.
+ * Using destructuring on the first argument is similar to doing const {user} = this.props in a class component.
+ */
+export default function Logout({user}) {
+    return (
+        <form onSubmit={e => e.preventDefault()}>
+            Logged in as: <b>{user}</b>
+            <input type={"submit"} value={"Logout"} />
+        </form>
+    )
+}
