92d4227c5f3307440ee1518ccf48b4df965f7c9a
Matty Wang <mattywang7@gmail.com>
2021-03-24 01:22:43 +0000

    User Login Component

---
+++ src/user/Login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+/**
+ * Two fields: username, password and a login button.
+ * Define a form container element to wrap them in.
+ *
+ * To avoid a page refresh when the form is submitted, we have to define an onSubmit handler and call:
+ * e.preventDefault() on the even object.
+ *
+ * Using semantic HTML elements such as <form> and <label> make your app
+ * easier to navigate for people using accessibility assistance software, such as screen readers.
+ * Further more, when using semantic HTML, keyboard shortcuts, such as submitting forms
+ * by pressing the return key, automatically work.
+ * @constructor
+ */
+export default function Login() {
+    return (
+        <form onSubmit={e => e.preventDefault()}>
+            <label htmlFor={"login-username"}>Username:</label>
+            <input type={"text"} name={"login-username"} id={"login-username"} />
+            <label htmlFor={"login-password"}>Password:</label>
+            <input type={"password"} name={"login-password"} id={"login-password"} />
+            <input type={"submit"} value={"Login"} />
+        </form>
+    );
+}
