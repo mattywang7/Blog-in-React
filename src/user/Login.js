b01843c13d8db1dbc9aca5f78a8ea9a8e94e8f14
Matty Wang <mattywang7@gmail.com>
2021-03-25 02:34:56 +0000

    Handler Username in Login

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 /**
  * Two fields: username, password and a login button.
@@ -13,14 +13,27 @@ import React from 'react';
  * by pressing the return key, automatically work.
  * @constructor
  */
-export default function Login() {
+export default function Login({setUser}) {
+    // define a new state Hook for the value of the Username field
+    const [username, setUsername] = useState('');
+
+    // Handler function for Username field
+    function handleUsername(evt) {
+        setUsername(evt.target.value);
+    }
+
     return (
-        <form onSubmit={e => e.preventDefault()}>
+        <form onSubmit={e => {e.preventDefault(); setUser(username)}}>
             <label htmlFor={"login-username"}>Username:</label>
-            <input type={"text"} name={"login-username"} id={"login-username"} />
+            <input type={"text"}
+                   value={username}
+                   onChange={handleUsername}
+                   name={"login-username"} id={"login-username"} />
             <label htmlFor={"login-password"}>Password:</label>
             <input type={"password"} name={"login-password"} id={"login-password"} />
-            <input type={"submit"} value={"Login"} />
+            <input type={"submit"}
+                   value={"Login"}
+                   disabled={username.length === 0} />
         </form>
     );
 }
